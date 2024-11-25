74d368b53666c10241a0b0871203c6f0ed13171b
Ismail Moghul <ismail.moghul@gmail.com>
2024-11-25 07:33:12 +0000

    update run script

--- run.sh
+++ run.sh
@@ -5,3 +5,13 @@ nextflow run ucl-medical-genomics/hygeia \
   --output_dir "s3://hygeia-analysis/results/1" \
   -bucket-dir "s3://hygeia-analysis/tmp/work/1" \
   -c aws.config -resume -with-tower
+
+# Beck Server
+# nextflow run main.nf \
+#   --cpg_file_path "s3://hygeia-analysis/data/ref/cpg.tsv.gz" \
+#   --sample_sheet "s3://hygeia-analysis/data/aging/sample_sheet.csv" \
+#   -bucket-dir "s3://hygeia-analysis/tmp/work/8" \
+#   -resume -with-tower \
+#   -c nextflow.config \
+#   -c aws.config \
+#   --output_dir "s3://hygeia-analysis/results/8"
